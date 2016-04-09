72fd27ca1e677aded49f491a172eca3f7278ce9e
Junichi Kakisako <kilo.sw@gmail.com>
2016-04-09 12:28:13 +0900

    add controller

---
+++ app/controllers/message_receives_controller.rb
@@ -0,0 +1,37 @@
+class MessageReceivesController < ApplicationController
+
+  protect_from_forgery with: :null_session
+
+  def callback
+    result = params[:result].first
+    from = result[:content][:from]
+    text = result[:content][:text]
+
+    response_message(from, text)
+    render json: [], status: :ok
+  end
+
+  private
+
+  def response_message(to, message)
+    RestClient.proxy = ENV['FIXIE_URL']
+    request_headers = {
+      "Content-Type": "application/json",
+      "X-Line-ChannelID": ENV['LINE_CHANNEL_ID'],
+      "X-Line-ChannelSecret": ENV['LINE_CHANNEL_SECRET'],
+      "X-Line-Trusted-User-With-ACL": ENV['LINE_CHANNEL_MID'],
+    }
+    request_params = {
+      to: [to],
+      toChannel: 1383378250,
+      eventType: "138311608800106203",
+      content: {
+        contentType: 1,
+        toType: 1,
+        text: message,
+      }
+    }
+    RestClient.post 'https://trialbot-api.line.me/v1/events' request_params.to_json, request_headers
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+
+  match 'message_receives/callback', :via => :post
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
